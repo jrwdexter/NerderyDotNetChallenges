c85f0f583c1cc42790eba1887e2318d7b58a26d3
Jonathan Dexter <jdexter@nerdery.com>
2015-05-12 18:17:01 -0500

    Initial challenges in fun literate format.

---
+++ app.fsx
@@ -0,0 +1,27 @@
+#r "packages/Suave/lib/net40/Suave.dll"
+open Suave
+open Suave.Http
+open Suave.Http.Applicatives
+open Suave.Http.Files
+open Suave.Http.Successful
+open Suave.Types
+open Suave.Web
+open System
+open System.Net
+open System.IO
+
+let challenges = DirectoryInfo(Path.Combine(__SOURCE_DIRECTORY__, "Challenges")).GetFiles()
+                 |> Seq.filter (fun f -> f.Extension = ".html")
+                 |> Seq.mapi (fun i c -> path (sprintf "/issue-%d" (i+1)) >>= file (sprintf "Challenges/%s" c.Name))
+                 |> Seq.toList
+
+let app : WebPart =
+  choose [
+    GET >>= choose (challenges |> List.append [
+      path "/styles.css" >>= file "template/fsharp-style.css" ;
+      path "/" >>= file "index.html" 
+      ] )
+  ]
+
+
+startWebServer defaultConfig app
---
+++ index.md
@@ -0,0 +1,16 @@
+# What this site is
+
+This is a simple site that exists to showcase building the [Nerdery .NET Challenges](https://dotnet.nerderylabs.com/) in F#.
+
+See the right section for each challenge and solution!
+
+## Building/Running each challenge
+
+Two steps:
+
+```PowerShell
+./paket.exe install
+console fsi Challenge_[DATE].fsx
+```
+
+That's it!
